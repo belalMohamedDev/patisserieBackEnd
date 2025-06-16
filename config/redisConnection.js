9c1334a7dc33859e1e41bdb77f7819fe5b779ff4
belal mohamed <belalagwa0@gmail.com>
2025-06-16 14:02:12 +0300

    test swagger

--- config/redisConnection.js
+++ config/redisConnection.js
@@ -1,26 +1,26 @@
-const { createClient } = require('redis');
+// const { createClient } = require('redis');
 
-const redisClient = createClient({
-  password: process.env.REDIS_PASSWORD || "",
-  socket: {
-    host: process.env.REDIS_HOST || "127.0.0.1",
-    port: process.env.REDIS_PORT || 6379,
-  },
-});
+// const redisClient = createClient({
+//   password: process.env.REDIS_PASSWORD || "",
+//   socket: {
+//     host: process.env.REDIS_HOST || "127.0.0.1",
+//     port: process.env.REDIS_PORT || 6379,
+//   },
+// });
 
-redisClient.on('connect', () => {
-  console.log('Connected to Redis');
-});
+// redisClient.on('connect', () => {
+//   console.log('Connected to Redis');
+// });
 
-redisClient.on('error', (err) => {
-  console.error('Redis connection error:', err);
-});
+// redisClient.on('error', (err) => {
+//   console.error('Redis connection error:', err);
+// });
 
-// Connect to Redis server
-redisClient.connect();
+// // Connect to Redis server
+// redisClient.connect();
 
 
 
-module.exports = redisClient;
+// module.exports = redisClient;
 
 
