bc0ee870151d2d6d18cf13ab7f618d6769c8ff11
Belal Mohamed <64976131+belalMohamedDev@users.noreply.github.com>
2025-10-23 20:42:28 +0300

    add active store

--- modules/storeAddressModel.js
+++ modules/storeAddressModel.js
@@ -35,6 +35,11 @@ const StoreAddressSchema = new mongoose.Schema(
       type: { type: String, enum: ["Point"], default: "Point" },
       coordinates: { type: [Number], default: [0, 0] },
     },
+    
+    active: {
+      type: Boolean,
+      default: true,
+    },
 
     deliveryZone: {
       type: {
